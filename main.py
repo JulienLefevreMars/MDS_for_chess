122d4fb8c2d709a2b001c870761ddd26f6ca2930
JulienLefevreMars <julien.lefevre@univ-amu.fr>
2021-06-09 16:41:39 +0200

    Eigenpairs (1/n)

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import numpy as np
 import sys
 
+def random_assymetric_matrix(n, draw_proba):
+	win_threshold = (1+draw_proba)/2 # assumes equal chances
+	# suboptimal, nxn random values instead of n(n-1)/2
+	A = np.random.rand(n,n)
+	A[A>win_threshold] = 1
+	A[np.logical_and(A<win_threshold,A>draw_proba)] = -1
+	A[np.logical_and(A>0,A<draw_proba)] = 0
+	A = np.triu(A,k=1)
+	A = A - A.transpose()
+	return A
+	
 
 def create_random_tournament(n,p = 1, draw_proba=0.05):
 	# INPUTS
@@ -12,20 +23,23 @@ def create_random_tournament(n,p = 1, draw_proba=0.05):
 	# M: n x n matrix with values between -p and p
 	
 	M = np.zeros((n,n))
-	win_threshold = (1+draw_proba)/2 # assumes equal chances
 	for game in range(p):
-		A = np.random.rand((n,n))
-		A[A>win_threshold] = 1
-		A[np.logical_and(A<win_threshold,A>draw_proba)] = -1
-		A[np.logical_and(A>0,A<draw_proba)] = 0
+		A = random_assymetric_matrix(n,draw_proba)
 		M = M + A
 	return M
 	
 
+	
+
 if __name__ == "__main__":
-	if len(sys.argv) == 1:
+	if len(sys.argv) >= 1:
 		n = 10
-	else:
+		p = 1
+	if len(sys.argv)>=2:
 		n = int(sys.argv[1])
+	if len(sys.argv)>=3:
+		p = int(sys.argv[2])
 	M = create_random_tournament(n,p)
-	print(M)
+	#print(M)
+	eigval, eigvec = np.linalg.eig(M) # complex eigenvalues but conjugate ! => 2D MDS is possible
+	#print(eigval)
